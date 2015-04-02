d050c8541b4bc880a9ced22cdcfce8740512809d
Nicole <nfelch121394@gmail.com>
2015-04-02 19:02:52 -0400

    REST api with CRUD functions

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from rest_framework import routers, serializers, viewsets
-from wisertogether.serializers import ResourceSerializer, DataSetSerializer
-from wisertogether.models import DataSet, Resource, Tag, Group
-
-class DataSetViewSet(viewsets.ModelViewSet):
-    queryset = DataSet.objects.all()
-    serializer_class = DataSetSerializer
-
-router = routers.DefaultRouter()
-router.register(r'data_sets', DataSetViewSet)
+from wisertogether import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^data_sets/$', views.DataSetList.as_view()),
+    url(r'^data_sets/(?P<pk>[0-9]+)/$', views.DataSetDetail.as_view()),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- wisertogether/views.py
+++ wisertogether/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from wisertogether.serializers import DataSetSerializer
+from wisertogether.models import DataSet
 
-# Create your views here.
+from rest_framework import generics
+
+class DataSetList(generics.ListCreateAPIView):
+    queryset = DataSet.objects.all()
+    serializer_class = DataSetSerializer
+
+
+class DataSetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DataSet.objects.all()
+    serializer_class = DataSetSerializer
